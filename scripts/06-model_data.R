fa247899112cf8567a2cf514939907368da102af
Victor Ma <victorjjma@gmail.com>
2024-12-03 19:32:04 -0500

    model section, scripts

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,37 +1,39 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Creates a logistic regression model for political preferences
+# Author: Victor Ma
+# Date: 3 December 2024
+# Contact: victo.ma@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+ces_data <- read_csv("data/02-analysis_data/analysis_data.csv")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+# Ensure categorical variables are correctly encoded
+ces_data$voted_for <- as_factor(ces_data$voted_for)
+ces_data$employment <- factor(ces_data$employment, levels = c("Full-time", "Part-time", "Temporarily laid off", "Unemployed", "Retired", "Permanently disabled", "Homemaker", "Student"))
+ces_data$income <- factor(ces_data$income, levels = c("< 10k", "10-50k", "50-100k", "100-200k", "200-500k", "> 500k"))
+ces_data$living <- factor(ces_data$living, levels = c("City", "Suburb", "Town", "Rural area"))
 
+#### Model data ####
+set.seed(853)
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+# Reducing the dataset for computational manageability
+ces_data_reduced <- ces_data |> 
+  slice_sample(n = 3000)
+
+# Specifying the logistic regression model
+political_preferences <- stan_glm(
+  voted_for ~ employment + income + living,
+  data = ces_data_reduced,
+  family = binomial(link = "logit"),
+  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  seed = 853
 )
 
-
+# Saving the model for future use
+saveRDS(political_preferences, file = "models/political_preferences.rds")
